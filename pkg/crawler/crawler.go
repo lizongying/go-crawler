72c775d4285e449112053d51e5c587f7079dce3b
Li ZongYing <lizongying@msn.com>
2023-08-30 21:56:04 +0800

    cron

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -161,7 +161,6 @@ func (c *Crawler) Start(ctx context.Context) (err error) {
 			}
 		}
 	case "cron":
-
 		re := regexp.MustCompile(`(?i)(\d+)([sihdmw])`)
 		r := re.FindStringSubmatch(c.spec)
 		if len(r) != 3 {
