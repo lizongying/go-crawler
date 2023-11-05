dd6067da2d73656378a5dec8520fd7d12af04f32
Li ZongYing <lizongying@msn.com>
2023-11-05 18:31:58 +0800

    change assets dir

--- web/ui/vite.config.js
+++ web/ui/vite.config.js
@@ -1,6 +1,6 @@
 import {fileURLToPath, URL} from 'node:url'
 
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import {AntDesignVueResolver} from 'unplugin-vue-components/resolvers';
@@ -8,29 +8,33 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        vueJsx(),
-        Components({
-            resolvers: [
-                AntDesignVueResolver({
-                    importStyle: false, // css in js
-                }),
-            ],
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    },
-    build: {
-        outDir: '../../static/dist',
-        rollupOptions: {
-            output: {
-                chunkFileNames: `assets/[hash].js`,
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd())
+    return {
+        plugins: [
+            vue(),
+            vueJsx(),
+            Components({
+                resolvers: [
+                    AntDesignVueResolver({
+                        importStyle: false, // css in js
+                    }),
+                ],
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+        build: {
+            outDir: '../../static/dist',
+            rollupOptions: {
+                output: {
+                    chunkFileNames: `assets/[hash].js`,
+                },
             },
+            assetsDir: env.VITE_ASSETS_DIR
         },
     }
 })
